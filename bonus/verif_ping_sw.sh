745e040293f9e596d15daca7f7eae388feda4c8b
Cheikh Ahmed Tidiane FALL <55579232+Bantou96@users.noreply.github.com>
2022-06-01 10:54:09 +0200

    Create verif_ping_sw.sh

---
+++ bonus/verif_ping_sw.sh
@@ -0,0 +1,17 @@
+$#/bin/bash
+
+for i in $(cat ip_sw)
+do
+                ping -c 2 $i
+                res=$(echo $?)
+                echo $res
+
+                if [[ $res == "1" ]]
+                then
+                        echo $i >> ping_sw_ko
+                else
+                        echo $i >> ping_sw_ok
+                fi
+
+done
+
